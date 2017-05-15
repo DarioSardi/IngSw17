23423bd9853861d4d615c25c49c01f925c6704cc
FrancescoSaverioVarini <ciao>
2017-05-15 23:29:53 +0200

    Modifiche a Initialization (in Board)

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,9 +1,11 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-	
+	final int excommunicationRound=2;
+	private int round;
 	private int era;
 	private List<Die> Dice;
 	private List territoryCardPool;
@@ -18,14 +20,68 @@ public class Board {
 	private FaithArea faithArea;
 	private List<Player> players;
 	
-	//TODO aspetta a generare getters e setters vien casino senno
+//TODO Controller class	
+//	private Controller c;
+	
+//TODO aspetta a generare getters e setters vien casino senno
+	
+	public Board(){
+		this.players= new ArrayList<Player>();
+		setPlayers();
+		//TODO
+		
+	}
+	
 	
 	public void initialize(){
+		createPlayerBoards();
+		setTowerCards();
+		setExcommunicationCards();
+		createMarket();
+		
+		
+//		askPlayersForDefaultBonus(Controller c);
+//		askPlayersForLeaderCards(Controller c);
+
+		
+		createAndRollDice();
+		
+		
+	}
+	private void setExcommunicationCards() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	private void createPlayerBoards() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void setTowerCards() {
+		getCards();
+		// TODO Auto-generated method stub
+		
+	}
+	private void getCards(){
+	//TODO shuffe cards and prepare all decks of cards
+	}
+	
+	private void createAndRollDice() {
+		// TODO Auto-generated method stub
 		
 	}
 	public void nextTurn(){
+		if(this.round == excommunicationRound){
+			
+			//TODO to implement scomunica
+		}
+		
 		
 	}
+	
+
 
 	
 
