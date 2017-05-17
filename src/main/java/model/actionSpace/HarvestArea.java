f98b88e496b40f026a1b6cc0bfa156d96bd90d51
DarioSardi <dario.sardi@mail.polimi.it>
2017-05-17 14:48:26 +0200

    updated ActionSpaces classes and moved to specific package

---
+++ src/main/java/model/actionSpace/HarvestArea.java
@@ -0,0 +1,29 @@
+package model.actionSpace;
+
+import model.FamilyMember;
+
+public class HarvestArea extends ActionSpace{
+	private FamilyMember FamiliarIn;
+	//DARIO generare costruttore
+	//DARIO logica relativa alle 2 aree
+	public FamilyMember getFamiliarIn() {
+		return FamiliarIn;
+	}
+
+	public void setFamiliarIn(FamilyMember familiarIn) {
+		FamiliarIn = familiarIn;
+	}
+	
+	public boolean check() {
+		return true; //DARIO compilare codice
+	}
+
+	public boolean execute() {
+		return true; //DARIO compilare codice
+	}
+
+
+
+ 
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
