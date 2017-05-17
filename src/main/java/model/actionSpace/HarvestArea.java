9225dc009d78c12f0640e00a3e7390d4a2826fbf
DARIO <DARIO@w10.fritz.box>
2017-05-18 00:13:07 +0200

    deleted useless methods in harvestArea

--- src/main/java/model/actionSpace/HarvestArea.java
+++ src/main/java/model/actionSpace/HarvestArea.java
@@ -4,34 +4,15 @@ import model.FamilyMember;
 import model.GlobalVariables;
 
 public class HarvestArea{
-	private FamilyMember FamiliarIn;
 	private SingleActionSpace baseZone;
 	private MultipleActionSpace extraZone;
 	
 	public HarvestArea(boolean enoughPlayers) {
-		super();
 		this.baseZone = new HarvestSingleActionSpace(GlobalVariables.minDiceValueHarvestArea, this);
 		this.extraZone = new HarvestMultipleActionSpace(GlobalVariables.minDiceValueHarvestArea, this);
 	}
-	//DARIO DA RIFARE D:
-	//DARIO generare costruttore
-	//DARIO logica relativa alle 2 aree
-	public FamilyMember getFamiliarIn() {
-		return FamiliarIn;
-	}
-
-	public void setFamiliarIn(FamilyMember familiarIn) {
-		FamiliarIn = familiarIn;
-	}
 	
-	public boolean check() {
-		return true; //DARIO compilare codice
-	}
 
-	public boolean execute() {
-		return true; //DARIO compilare codice
-	}
-	
 	/**
 	 * check if the player is already in one of the area
 	 * @param f family member that is trying  to enter
