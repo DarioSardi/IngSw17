64988e02a3b08369ff6fa4a5310d5e5ba5fe5fce
FrancescoSaverioVarini <ciao>
2017-05-24 20:07:45 +0200

    Cost effect costruttore aggiornato

--- src/main/java/it/polimi/ingsw/GC_43/model/effects/CostEffect.java
+++ src/main/java/it/polimi/ingsw/GC_43/model/effects/CostEffect.java
@@ -1,14 +1,15 @@
 package it.polimi.ingsw.GC_43.model.effects;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import it.polimi.ingsw.GC_43.model.Player;
 import it.polimi.ingsw.GC_43.model.Resource;
 
 public class CostEffect extends Effect {
-	private HashMap<String, Resource> Costs;
+	private ArrayList<Resource> Costs;
 	
-	public CostEffect(){
-		this.Costs= new HashMap<String, Resource>();
+	public CostEffect(ArrayList<Resource> Costs){
+		this.Costs= Costs;
 	}
 	
 	public boolean check(Player player, boolean towerTax){
