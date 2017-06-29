62f1adac0b5b3f43d203d03a7573dd6451b46409
SapuppoSamuel <10454582@polimi.it>
2017-06-29 21:22:27 +0200

    problem solved

--- src/main/java/it/polimi/ingsw/GC_43/model/PlayerPersonalBonus.java
+++ src/main/java/it/polimi/ingsw/GC_43/model/PlayerPersonalBonus.java
@@ -12,31 +12,29 @@ public class PlayerPersonalBonus implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -2815971959175413082L;
-	private ArrayList<BuildingCard> buildingCards;
-	
-	public PlayerPersonalBonus (ArrayList<Effect> productionBonus, ArrayList<Effect> advancedBonus){
-		this.buildingCards = new ArrayList<>();
+	private ArrayList<Effect> productionBonus;
+	private ArrayList<Effect> harvestBonus;
+	public PlayerPersonalBonus (ArrayList<Effect> productionBonus, ArrayList<Effect> harvestBonus){
+		this.productionBonus = productionBonus;
+		this.harvestBonus = harvestBonus;
 	}
 	
-	public BuildingCard getBuildingCard(int numCarta){
-		return this.buildingCards.get(numCarta);	
+	public ArrayList<Effect> getProductionBonus(){
+		return this.productionBonus;	
 	}
 	
-	public ArrayList<BuildingCard> getArrayBuildingCards(){
-		return this.buildingCards;	
-	}
-		
-	public void addBuildingCard(BuildingCard card){
-		this.buildingCards.add(card);	
-	}
-
-	
-	public boolean canIAdd(int actualNumCards){
-		return (actualNumCards <= GlobalVariables.maxNumberPlayerCards);
+	public ArrayList<Effect> getHarvestBonus(){
+		return this.harvestBonus;	
 	}
 
 	public String toString(){
-		String s="";	
+		String s="";
+		s = s + "ProductionBonus: \n";
+		for (int i=0; i < this.productionBonus.size(); i++)
+			s = s + this.productionBonus.get(i).toString();
+		s = s + "HarvestBonus: \n";
+		for (int i=0; i < this.harvestBonus.size(); i++)
+			s = s + this.harvestBonus.get(i).toString();
 		return s;
 	}
 }
